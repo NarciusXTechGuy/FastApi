21e59b498eea60691075fe92d1205eeadd084669
NarciusXTechGuy <tenwinjames5@gmail.com>
2022-10-18 18:06:13 +0530

    commit

--- main.py
+++ main.py
@@ -10,3 +10,26 @@ def index(): #path operation function
 @app.get('/about')
 def about():
     return {'data':'about page'}
+
+@app.get('/blog') #working with the query set eg: /blog?limit=50&published=true
+def blog(limit,published):
+    if published:
+        return {'data':f'{limit} published blogs from db'}
+    else:
+        return {'data':f'{limit} unpublished blogs from db'}
+        
+
+
+@app.get('blog/unpublished')#string 
+def unpublished(): #same name passed in down also only parameters changed one is string and another is integer. line by line execution
+    return {'data':'unpublished data'}
+
+@app.get('/blog/{id}') # example /blog/1 or /blog/2 #integer
+def show(id:int): # defining type (key:type)
+    return {'data':id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data':[id,'1',id]}
+
+
